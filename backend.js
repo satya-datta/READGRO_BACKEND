43b2a18ecc34c0681056b73e5c500153c90a76de
Satya <satyadattakallepalli@gmail.com>
2025-03-15 11:44:05 +0530

    before hosting ckeck

--- backend.js
+++ backend.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "admin",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12767778",
+  password: "iA26b8kBLp",
+  database: "sql12767778",
 });
 module.exports = connection;
--- connection2.js
+++ connection2.js
@@ -1,14 +1,16 @@
 const mysql = require("mysql2"); // Use mysql2 for Promises
 
-const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "admin",
+const connection = mysql
+  .createPool({
+    host: "sql12.freesqldatabase.com",
+    user: "sql12767778",
+    password: "iA26b8kBLp",
+    database: "sql12767778",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-}).promise(); // Enable async/await
+  })
+  .promise(); // Enable async/await
 
 console.log("✅ Connected to MySQL (Promise-based Connection)");
 
