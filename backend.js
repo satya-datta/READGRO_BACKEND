c88be476fe3ccc1755c6cbd0750b10e1b8f12bab
Satya <satyadattakallepalli@gmail.com>
2025-04-01 16:36:55 +0530

    adding cors url  for api

--- backend.js
+++ backend.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  // host: "sql12.freesqldatabase.com",
-  // user: "sql12767778",
-  // password: "iA26b8kBLp",
-  // database: "sql12767778",
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "admin",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12770685",
+  password: "wbctVpAztd",
+  database: "sql12770685",
+  // host: "localhost",
+  // user: "root",
+  // password: "root",
+  // database: "admin",
 });
 module.exports = connection;
