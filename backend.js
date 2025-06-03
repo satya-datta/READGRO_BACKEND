6527711f42071124d9d754d287ca0f01c92f1529
Satya <satyadattakallepalli@gmail.com>
2025-06-03 15:54:45 +0530

    changing db

--- backend.js
+++ backend.js
@@ -25,21 +25,34 @@
 // connection.js (updated)
 const mysql = require("mysql2");
 
+// const pool = mysql.createPool({
+//   // host: "switchyard.proxy.rlwy.net",
+//   // user: "root",
+//   // password: "TuIPVtJfAolyDxCjDOGqGLNkiTOrgKYs",
+//   // database: "railway",
+//   // port: 51681,
+//   host: "database-1.c0jsqu0427x8.us-east-1.rds.amazonaws.com",
+//   user: "admin",
+//   password: "Readgro123",
+//   database: "admin",
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10, // Adjust based on your DB plan
+//   queueLimit: 0,
+// });
 const pool = mysql.createPool({
-  // host: "switchyard.proxy.rlwy.net",
-  // user: "root",
-  // password: "TuIPVtJfAolyDxCjDOGqGLNkiTOrgKYs",
-  // database: "railway",
-  // port: 51681,
-  host: "database-1.c0jsqu0427x8.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "Readgro123",
-  database: "admin",
+  host: "maglev.proxy.rlwy.net",
+  user: "root",
+  password: "MyClMlQUmYJFArWvldbKwylcKATRcMZJ",
+  database: "railway",
+  port: 42802,
+ 
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10, // Adjust based on your DB plan
   queueLimit: 0,
 });
+
 
 // Test connection
 pool.getConnection((err, conn) => {
